85623eac645258c21073b3c08a5f03affaaed3d9
danfennellftw <dan.fennell@gmail.com>
2026-03-31 21:01:56 -0700

    Make server port configurable and disable reload
    Change the server port to be configurable via environment variable and disable reload.

--- app/main.py
+++ app/main.py
@@ -27,4 +27,7 @@ def docs_redirect():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
